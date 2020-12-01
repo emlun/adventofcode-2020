514986389b35441e52e019c31cc3c51ea6d1ca88
Emil Lundberg <emil@emlun.se>
2020-12-01 07:21:11 +0100

    Optimize day 1

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -1,39 +1,45 @@
 use crate::common::Solution;
+use std::collections::HashSet;
 
-fn solve_a(lines: &[String]) -> i32 {
-    let numbers: Vec<i32> = lines
-        .iter()
-        .map(|line| line.parse::<i32>().unwrap())
-        .collect();
+const TARGET: i32 = 2020;
 
-    for a in &numbers {
-        for b in &numbers {
-            if a != b && a + b == 2020 {
-                return a * b;
-            }
+fn solve_a(numbers: &[i32], numset: &HashSet<i32>) -> i32 {
+    for a in numbers {
+        let b = TARGET - a;
+        if numset.contains(&b) {
+            return a * b;
         }
     }
-    unreachable!()
+    unreachable!();
 }
 
-fn solve_b(lines: &[String]) -> i32 {
-    let numbers: Vec<i32> = lines
-        .iter()
-        .map(|line| line.parse::<i32>().unwrap())
-        .collect();
-
-    for a in &numbers {
-        for b in &numbers {
-            for c in &numbers {
-                if a != b && b != c && a != c && a + b + c == 2020 {
+fn solve_b(numbers: &[i32], numset: &HashSet<i32>) -> i32 {
+    for (ia, a) in numbers.iter().enumerate() {
+        for b in &numbers[(ia + 1)..] {
+            let ab = a + b;
+            if ab < TARGET {
+                let c = TARGET - ab;
+                if numset.contains(&c) {
                     return a * b * c;
                 }
+            } else {
+                continue;
             }
         }
     }
-    unreachable!()
+    unreachable!();
 }
 
 pub fn solve(lines: &[String]) -> Solution {
-    (solve_a(lines).to_string(), solve_b(lines).to_string())
+    let mut numbers: Vec<i32> = lines
+        .iter()
+        .map(|line| line.parse::<i32>().unwrap())
+        .collect();
+    numbers.sort();
+    let numset: HashSet<i32> = numbers.iter().copied().collect();
+
+    (
+        solve_a(&numbers, &numset).to_string(),
+        solve_b(&numbers, &numset).to_string(),
+    )
 }
