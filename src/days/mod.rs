e906377ea3a740afcde2cca3cc4cc465168ab4c6
Emil Lundberg <emil@emlun.se>
2020-12-01 08:06:19 +0100

    Use macro to enumerate day solvers

--- src/days/mod.rs
+++ src/days/mod.rs
@@ -1,12 +1,18 @@
-pub mod day01;
+macro_rules! days {
+    ($($day_mod:ident),*) => {
+        $(pub mod $day_mod;)*
+
+        pub fn get_solver(day: u8) -> Option<fn(&[String]) -> crate::common::Solution> {
+            match format!("day{:02}", day).as_str() {
+                $(stringify!($day_mod) => Some($day_mod::solve),)*
+                _ => None,
+            }
+        }
+    };
+}
 
 pub fn all_numbers() -> Vec<u8> {
     (1..=25).filter(|&day| get_solver(day).is_some()).collect()
 }
 
-pub fn get_solver(day: u8) -> Option<fn(&[String]) -> crate::common::Solution> {
-    match day {
-        1 => Some(day01::solve),
-        _ => None,
-    }
-}
+days!(day01);
