40ba6312b28dd094a0b77518c8c08893b5cfacda
Emil Lundberg <emil@emlun.se>
2020-12-03 06:27:50 +0100

    Clean up day 3

--- src/days/day03.rs
+++ src/days/day03.rs
@@ -1,37 +1,34 @@
 use crate::common::Solution;
 
-fn solve_a(map: &Vec<Vec<char>>) -> usize {
+fn count(map: &[String], dx: usize, dy: usize) -> usize {
     let w = map[0].len();
     let mut count = 0;
-    for i in 0..map.len() {
-        let x = (3 * i) % w;
-        let y = i;
-        if map[y][x] == '#' {
+    for i in 0..(map.len() / dy) {
+        let x = (dx * i) % w;
+        let y = dy * i;
+        if map[y].chars().nth(x) == Some('#') {
             count += 1;
         }
     }
     count
 }
 
-fn solve_b(map: &Vec<Vec<char>>) -> usize {
-    let w = map[0].len();
-    let mut product = 1;
-    for (dx, dy) in vec![(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)] {
-        let mut count = 0;
-        for i in 0..(map.len() / dy) {
-            let x = (dx * i) % w;
-            let y = dy * i;
-            if map[y][x] == '#' {
-                count += 1;
-            }
-        }
-        product *= count;
-    }
-    product
+fn solve_a(map: &[String]) -> usize {
+    count(map, 3, 1)
+}
+
+fn solve_b(map: &[String], a_solution: usize) -> usize {
+    let p: usize = vec![(1, 1), (5, 1), (7, 1), (1, 2)]
+        .iter()
+        .map(|(dx, dy)| count(map, *dx, *dy))
+        .product();
+    p * a_solution
 }
 
 pub fn solve(lines: &[String]) -> Solution {
-    let map: Vec<Vec<char>> = lines.iter().map(|line| line.chars().collect()).collect();
-
-    (solve_a(&map).to_string(), solve_b(&map).to_string())
+    let a_solution = solve_a(lines);
+    (
+        a_solution.to_string(),
+        solve_b(lines, a_solution).to_string(),
+    )
 }
