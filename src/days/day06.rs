d7a91a9c4ba5e4eb0b8b8962bfef39b6d6a5a1c6
Emil Lundberg <emil@emlun.se>
2020-12-06 06:26:40 +0100

    Solve day 6b

--- src/days/day06.rs
+++ src/days/day06.rs
@@ -1,5 +1,4 @@
 use crate::common::Solution;
-use std::collections::HashMap;
 use std::collections::HashSet;
 
 fn solve_a(groups: &[HashSet<char>]) -> usize {
@@ -7,14 +6,38 @@ fn solve_a(groups: &[HashSet<char>]) -> usize {
 }
 
 pub fn solve(lines: &[String]) -> Solution {
-    let groups: Vec<HashSet<char>> = lines.iter().fold(vec![HashSet::new()], |mut result, line| {
-        if line == "" {
-            result.push(HashSet::new());
-        } else {
-            result.last_mut().unwrap().extend(line.chars());
-        }
-        result
-    });
+    let mut lines: Vec<&String> = lines.iter().skip_while(|l| l.is_empty()).collect();
+    while lines[lines.len() - 1].is_empty() {
+        lines.pop();
+    }
+    let lines = lines;
 
-    (solve_a(&groups).to_string(), "".to_string())
+    let groups_a: Vec<HashSet<char>> =
+        lines.iter().fold(vec![HashSet::new()], |mut result, line| {
+            if line.is_empty() {
+                result.push(HashSet::new());
+            } else {
+                result.last_mut().unwrap().extend(line.chars());
+            }
+            result
+        });
+
+    let groups_b: Vec<HashSet<char>> =
+        lines
+            .iter()
+            .fold(vec![('a'..='z').collect()], |mut result, line| {
+                if line.is_empty() {
+                    result.push(('a'..='z').collect());
+                } else {
+                    let person_ans: HashSet<char> = line.chars().collect();
+                    let last = result.last_mut().unwrap();
+                    last.retain(|c| person_ans.contains(c));
+                }
+                result
+            });
+
+    (
+        solve_a(&groups_a).to_string(),
+        solve_a(&groups_b).to_string(),
+    )
 }
