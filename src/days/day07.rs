80497cdf4107c63fcec260028ea846002e11fed6
Emil Lundberg <emil@emlun.se>
2020-12-07 06:51:35 +0100

    Solve day 7b

--- src/days/day07.rs
+++ src/days/day07.rs
@@ -3,7 +3,7 @@ use std::collections::HashMap;
 use std::collections::HashSet;
 use std::collections::VecDeque;
 
-fn solve_a(inverse_rules: HashMap<String, HashMap<String, usize>>) -> usize {
+fn solve_a(inverse_rules: &HashMap<String, HashMap<String, usize>>) -> usize {
     let mut seen: HashSet<&str> = HashSet::new();
     let mut queue: VecDeque<&str> = VecDeque::new();
     queue.push_back("shiny gold");
@@ -15,6 +15,19 @@ fn solve_a(inverse_rules: HashMap<String, HashMap<String, usize>>) -> usize {
         }
     }
     seen.len() - 1
+}
+
+fn solve_b(rules: &HashMap<String, HashMap<String, usize>>, color: &str) -> usize {
+    dbg!(color);
+    1 + if let Some(contents) = rules.get(color) {
+        dbg!(contents);
+        contents
+            .iter()
+            .map(|(content_color, num)| num * solve_b(rules, content_color))
+            .sum()
+    } else {
+        0
+    }
 }
 
 pub fn solve(lines: &[String]) -> Solution {
@@ -63,5 +76,8 @@ pub fn solve(lines: &[String]) -> Solution {
     }
     println!("{:?}", inverse_rules);
 
-    (solve_a(inverse_rules).to_string(), "".to_string())
+    (
+        solve_a(&inverse_rules).to_string(),
+        (solve_b(&rules, "shiny gold") - 1).to_string(),
+    )
 }
