0901310f40ac7eef9381e2a3c58b3eb3f5deef43
Emil Lundberg <emil@emlun.se>
2020-12-07 07:01:09 +0100

    Avoid cloning strings in day 7

--- src/days/day07.rs
+++ src/days/day07.rs
@@ -3,20 +3,22 @@ use std::collections::HashMap;
 use std::collections::HashSet;
 use std::collections::VecDeque;
 
-fn solve_a(inverse_rules: &HashMap<String, HashMap<String, usize>>) -> usize {
-    let mut seen: HashSet<&str> = HashSet::new();
-    let mut queue: VecDeque<&str> = VecDeque::new();
-    queue.push_back("shiny gold");
+type Color<'a> = (&'a str, &'a str);
+
+fn solve_a(inverse_rules: &HashMap<Color, HashMap<Color, usize>>) -> usize {
+    let mut seen: HashSet<&Color> = HashSet::new();
+    let mut queue: VecDeque<&Color> = VecDeque::new();
+    queue.push_back(&("shiny", "gold"));
     while let Some(color) = queue.pop_front() {
         seen.insert(color);
         if let Some(containers) = inverse_rules.get(color) {
-            queue.extend(containers.keys().map(|s| s.as_str()));
+            queue.extend(containers.keys());
         }
     }
     seen.len() - 1
 }
 
-fn solve_b(rules: &HashMap<String, HashMap<String, usize>>, color: &str) -> usize {
+fn solve_b(rules: &HashMap<Color, HashMap<Color, usize>>, color: &Color) -> usize {
     1 + if let Some(contents) = rules.get(color) {
         contents
             .iter()
@@ -28,30 +30,34 @@ fn solve_b(rules: &HashMap<String, HashMap<String, usize>>, color: &str) -> usiz
 }
 
 pub fn solve(lines: &[String]) -> Solution {
-    let mut rules: HashMap<String, HashMap<String, usize>> = HashMap::new();
-    let mut inverse_rules: HashMap<String, HashMap<String, usize>> = HashMap::new();
+    let mut rules: HashMap<Color, HashMap<Color, usize>> = HashMap::new();
+    let mut inverse_rules: HashMap<Color, HashMap<Color, usize>> = HashMap::new();
 
     for line in lines {
         let mut words = line.split(' ');
-        let container_color = format!("{} {}", words.next().unwrap(), words.next().unwrap());
+        let container_color = (words.next().unwrap(), words.next().unwrap());
         words.next();
         words.next();
 
-        let mut new_rules: HashMap<String, usize> = HashMap::new();
+        let mut new_rules: HashMap<Color, usize> = HashMap::new();
 
         while let Some(num) = words.next() {
             if let Ok(num) = num.parse::<usize>() {
-                let content_color = format!("{} {}", words.next().unwrap(), words.next().unwrap())
-                    .trim_end_matches(',')
-                    .trim_end_matches('.')
-                    .to_string();
+                let content_color = (
+                    words.next().unwrap(),
+                    words
+                        .next()
+                        .unwrap()
+                        .trim_end_matches(',')
+                        .trim_end_matches('.'),
+                );
                 words.next();
 
-                new_rules.insert(content_color.clone(), num);
+                new_rules.insert(content_color, num);
                 inverse_rules
                     .entry(content_color)
                     .or_insert(HashMap::new())
-                    .insert(container_color.clone(), num);
+                    .insert(container_color, num);
             } else {
                 words.next();
                 words.next();
@@ -63,6 +69,6 @@ pub fn solve(lines: &[String]) -> Solution {
 
     (
         solve_a(&inverse_rules).to_string(),
-        (solve_b(&rules, "shiny gold") - 1).to_string(),
+        (solve_b(&rules, &("shiny", "gold")) - 1).to_string(),
     )
 }
