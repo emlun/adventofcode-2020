dd90662d274dbdf0b5f79bcf50a531f28e0acd6d
Emil Lundberg <emil@emlun.se>
2020-12-08 07:01:57 +0100

    Clean up day 8

--- src/days/day08.rs
+++ src/days/day08.rs
@@ -3,86 +3,99 @@ use std::collections::HashSet;
 use std::convert::TryFrom;
 use std::convert::TryInto;
 
-fn solve_a(program: &[(&str, i32)]) -> i32 {
+type Word = i32;
+enum Instruction {
+    Nop(Word),
+    Acc(Word),
+    Jmp(Word),
+}
+type Program = Vec<Instruction>;
+use Instruction::*;
+
+fn parse(lines: &[String]) -> Program {
+    lines
+        .iter()
+        .filter(|l| !l.is_empty())
+        .map(|l| {
+            let mut words = l.split(' ');
+            let instr = words.next().unwrap();
+            let arg = words.next().unwrap().parse().unwrap();
+            match instr {
+                "nop" => Nop(arg),
+                "acc" => Acc(arg),
+                "jmp" => Jmp(arg),
+                _ => unimplemented!(),
+            }
+        })
+        .collect()
+}
+
+fn solve_a(program: &Program) -> Word {
     let mut acc = 0;
     let mut eip = 0;
-    let mut history: HashSet<i32> = HashSet::new();
+    let mut history: HashSet<Word> = HashSet::new();
+
     loop {
-        if !history.insert(eip.try_into().unwrap()) {
+        if !history.insert(eip) {
             break acc;
         }
 
         match program[usize::try_from(eip).unwrap()] {
-            ("nop", _) => {}
-            ("acc", d) => {
+            Nop(_) => {}
+            Acc(d) => {
                 acc += d;
             }
-            ("jmp", d) => {
+            Jmp(d) => {
                 eip += d - 1;
             }
-            _ => unreachable!(),
         };
         eip += 1;
     }
 }
 
-fn solve_b(program: &[(&str, i32)]) -> i32 {
-    for corruption_index in (0..program.len()).filter(|i| {
-        let inst = program[*i].0;
-        inst == "nop" || inst == "jmp"
+fn solve_b(program: &Program) -> Word {
+    for corruption_index in (0..program.len()).filter(|i| match program[*i] {
+        Nop(_) | Jmp(_) => true,
+        _ => false,
     }) {
-        let mut history: HashSet<i32> = HashSet::new();
-        let mut acc: i32 = 0;
-        let mut eip: i32 = 0;
-        let (found, result) = loop {
+        let mut history: HashSet<Word> = HashSet::new();
+        let mut acc: Word = 0;
+        let mut eip: Word = 0;
+
+        loop {
             let eip_usize: usize = eip.try_into().unwrap();
             if eip_usize == program.len() {
-                break (true, acc);
+                return acc;
             } else if !history.insert(eip) {
-                break (false, acc);
+                break;
             } else {
                 match program[eip_usize] {
-                    ("nop", d) => {
+                    Nop(d) => {
                         if eip_usize == corruption_index {
                             eip += d;
                         } else {
                             eip += 1;
                         }
                     }
-                    ("acc", d) => {
+                    Acc(d) => {
                         acc += d;
                         eip += 1;
                     }
-                    ("jmp", d) => {
+                    Jmp(d) => {
                         if eip_usize == corruption_index {
                             eip += 1;
                         } else {
                             eip += d;
                         }
                     }
-                    _ => unreachable!(),
                 };
             }
-        };
-        if found {
-            return result;
         }
     }
     unreachable!()
 }
 
 pub fn solve(lines: &[String]) -> Solution {
-    let program: Vec<(&str, i32)> = lines
-        .iter()
-        .filter(|l| !l.is_empty())
-        .map(|l| {
-            let mut words = l.split(' ');
-            (
-                words.next().unwrap(),
-                words.next().unwrap().parse().unwrap(),
-            )
-        })
-        .collect();
-
+    let program: Program = parse(lines);
     (solve_a(&program).to_string(), solve_b(&program).to_string())
 }
