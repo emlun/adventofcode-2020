cb121e49b45563d22ac9835c395d7c14f038c3b7
Emil Lundberg <emil@emlun.se>
2020-12-09 06:21:32 +0100

    Solve day 9b

--- src/days/day09.rs
+++ src/days/day09.rs
@@ -6,7 +6,7 @@ use std::convert::TryInto;
 const WINDOW: isize = 25;
 
 fn solve_a(nums: &[isize]) -> isize {
-    let mut set: BTreeSet<isize> = nums[0..WINDOW].iter().copied().collect();
+    let mut set: BTreeSet<isize> = nums[0..WINDOW as usize].iter().copied().collect();
     let mut i: isize = 0;
     loop {
         let j = nums[(i + WINDOW) as usize];
@@ -20,11 +20,29 @@ fn solve_a(nums: &[isize]) -> isize {
     }
 }
 
+fn solve_b(nums: &[isize], a_solution: isize) -> isize {
+    let mut set: BTreeSet<isize> = nums[0..WINDOW as usize].iter().copied().collect();
+    let mut i: isize = 0;
+    for seqlen in 2.. {
+        for i in 0..(nums.len() - seqlen) {
+            let imax = i + seqlen;
+            if nums[i..imax].iter().sum::<isize>() == a_solution {
+                return nums[i..imax].iter().min().unwrap() + nums[i..imax].iter().max().unwrap();
+            }
+        }
+    }
+    unreachable!()
+}
+
 pub fn solve(lines: &[String]) -> Solution {
     let nums: Vec<isize> = lines
         .iter()
         .filter(|l| !l.is_empty())
         .map(|l| l.parse().unwrap())
         .collect();
-    (solve_a(&nums).to_string(), "".to_string())
+    let a_solution = solve_a(&nums);
+    (
+        a_solution.to_string(),
+        solve_b(&nums, a_solution).to_string(),
+    )
 }
