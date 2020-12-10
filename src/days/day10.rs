d0182b810277ad269474fa7571b08f34e5ec07b0
Emil Lundberg <emil@emlun.se>
2020-12-10 06:37:14 +0100

    Solve day 10b

--- src/days/day10.rs
+++ src/days/day10.rs
@@ -1,7 +1,4 @@
 use crate::common::Solution;
-use std::collections::BTreeSet;
-
-const WINDOW: usize = 25;
 
 fn solve_a(nums: &[usize]) -> usize {
     let mut diff1 = 0;
@@ -16,6 +13,31 @@ fn solve_a(nums: &[usize]) -> usize {
     diff1 * diff3
 }
 
+fn solve_b(nums: &[usize]) -> usize {
+    if nums.is_empty() {
+        0
+    } else if nums.len() == 1 {
+        1
+    } else {
+        let midi = nums.len() / 2;
+
+        let front = &nums[0..midi];
+        let back = &nums[midi..];
+
+        let mut arrangements = 0;
+
+        for fronti in 0..std::cmp::min(3, front.len()) {
+            for backi in 0..std::cmp::min(3, back.len()) {
+                if back[backi] - front[front.len() - fronti - 1] <= 3 {
+                    arrangements +=
+                        solve_b(&front[0..=front.len() - fronti - 1]) * solve_b(&back[backi..]);
+                }
+            }
+        }
+        arrangements
+    }
+}
+
 pub fn solve(lines: &[String]) -> Solution {
     let mut nums: Vec<usize> = lines
         .iter()
@@ -26,5 +48,5 @@ pub fn solve(lines: &[String]) -> Solution {
     nums.sort_unstable();
     nums.push(nums.last().unwrap() + 3);
     let a_solution = solve_a(&nums);
-    (a_solution.to_string(), "".to_string())
+    (a_solution.to_string(), solve_b(&nums).to_string())
 }
