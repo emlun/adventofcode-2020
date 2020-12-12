8d7910295270ca92c0e5e91361a68dab077d73d1
Emil Lundberg <emil@emlun.se>
2020-12-12 06:33:12 +0100

    Solve day 12b

--- src/days/day12.rs
+++ src/days/day12.rs
@@ -25,6 +25,43 @@ fn solve_a(instructions: &[(char, i32)]) -> i32 {
     ship_x.abs() + ship_y.abs()
 }
 
+fn solve_b(instructions: &[(char, i32)]) -> i32 {
+    let mut ship_x = 0;
+    let mut ship_y = 0;
+
+    let mut wp_dx = 10;
+    let mut wp_dy = 1;
+
+    for (inst_dir, inst_len) in instructions {
+        match inst_dir {
+            'N' => wp_dy += inst_len,
+            'S' => wp_dy -= inst_len,
+            'E' => wp_dx += inst_len,
+            'W' => wp_dx -= inst_len,
+            'L' => {
+                for _ in 0..(inst_len / 90) {
+                    let tmp = (-wp_dy, wp_dx);
+                    wp_dx = tmp.0;
+                    wp_dy = tmp.1;
+                }
+            }
+            'R' => {
+                for _ in 0..(inst_len / 90) {
+                    let tmp = (wp_dy, -wp_dx);
+                    wp_dx = tmp.0;
+                    wp_dy = tmp.1;
+                }
+            }
+            'F' => {
+                ship_x += wp_dx * inst_len;
+                ship_y += wp_dy * inst_len;
+            }
+            _ => unreachable!(),
+        }
+    }
+    ship_x.abs() + ship_y.abs()
+}
+
 pub fn solve(lines: &[String]) -> Solution {
     let instructions: Vec<(char, i32)> = lines
         .iter()
@@ -36,5 +73,8 @@ pub fn solve(lines: &[String]) -> Solution {
             )
         })
         .collect();
-    (solve_a(&instructions).to_string(), "".to_string())
+    (
+        solve_a(&instructions).to_string(),
+        solve_b(&instructions).to_string(),
+    )
 }
