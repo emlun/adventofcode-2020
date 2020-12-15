fbeb8ceaf1b4ec0f8b426b25a71eca85547d4135
Emil Lundberg <emil@emlun.se>
2020-12-15 07:19:24 +0100

    Use for loop instead of loop loop

--- src/days/day15.rs
+++ src/days/day15.rs
@@ -2,19 +2,15 @@ use crate::common::Solution;
 
 fn solve_for(lines: &[String], target: usize) -> usize {
     let init: Vec<usize> = lines[0].split(',').map(|s| s.parse().unwrap()).collect();
-    let mut turn = init.len() - 1;
 
-    let mut next: usize = init[turn];
+    let turn0 = init.len() - 1;
+    let mut next: usize = *init.last().unwrap();
     let mut last_seen: Vec<Option<usize>> = vec![None; target];
     for (i, n) in init.into_iter().enumerate() {
         last_seen[n] = Some(i);
     }
 
-    loop {
-        if turn + 1 == target {
-            return next;
-        }
-
+    for turn in turn0..target - 1 {
         if let Some(last_seen_at) = &mut last_seen[next] {
             next = turn - *last_seen_at;
             *last_seen_at = turn;
@@ -22,9 +18,8 @@ fn solve_for(lines: &[String], target: usize) -> usize {
             last_seen[next] = Some(turn);
             next = 0;
         }
-
-        turn += 1;
     }
+    next
 }
 
 fn solve_a(lines: &[String]) -> usize {
