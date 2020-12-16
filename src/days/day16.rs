b11e6cb62c5f9138ab07e101b10f8d6c56d2ae86
Emil Lundberg <emil@emlun.se>
2020-12-16 06:57:04 +0100

    Solve day 16b

--- src/days/day16.rs
+++ src/days/day16.rs
@@ -1,23 +1,114 @@
 use crate::common::Solution;
 use std::collections::HashMap;
+use std::collections::HashSet;
 use std::ops::RangeInclusive;
 
 fn solve_a(
     rules: &HashMap<&str, Vec<RangeInclusive<usize>>>,
-    your_ticket: &Vec<usize>,
     other_tickets: &Vec<Vec<usize>>,
 ) -> usize {
     other_tickets
         .iter()
         .flat_map(|t| t.iter())
         .filter(|n| {
-            dbg!(n);
             !rules
                 .values()
                 .flatten()
                 .any(|allowed: &RangeInclusive<usize>| allowed.contains(*n))
         })
         .sum()
+}
+
+fn discard_invalid(
+    rules: &HashMap<&str, Vec<RangeInclusive<usize>>>,
+    tickets: &Vec<Vec<usize>>,
+) -> Vec<Vec<usize>> {
+    tickets
+        .iter()
+        .filter(|ticket| {
+            ticket.iter().all(|n| {
+                rules
+                    .values()
+                    .flatten()
+                    .any(|allowed: &RangeInclusive<usize>| allowed.contains(n))
+            })
+        })
+        .cloned()
+        .collect()
+}
+
+fn solve_b(
+    rules: &HashMap<&str, Vec<RangeInclusive<usize>>>,
+    your_ticket: &Vec<usize>,
+    other_tickets: &Vec<Vec<usize>>,
+) -> usize {
+    let valid_other_tickets = discard_invalid(rules, other_tickets);
+
+    let mut field_mapping: HashMap<usize, HashSet<&str>> = (0..)
+        .take_while(|i| valid_other_tickets.iter().any(|t| t.len() >= *i))
+        .map(|i| {
+            let mut possible_fields: Vec<HashSet<&str>> = valid_other_tickets
+                .iter()
+                .flat_map(|ticket| ticket.get(i))
+                .map(|field_value| {
+                    let possible_fields_for_this_value = rules
+                        .keys()
+                        .filter(|k| {
+                            rules
+                                .get(*k)
+                                .unwrap()
+                                .iter()
+                                .any(|rule| rule.contains(field_value))
+                        })
+                        .map(|k| *k)
+                        .collect();
+                    possible_fields_for_this_value
+                })
+                .collect();
+
+            (
+                i,
+                possible_fields.pop().map(|first| {
+                    possible_fields.into_iter().fold(first, |isct, next| {
+                        isct.intersection(&next).copied().collect()
+                    })
+                }),
+            )
+        })
+        .flat_map(|(k, v)| v.map(|v| (k, v)))
+        .collect();
+
+    let field_keys: HashSet<usize> = field_mapping.keys().copied().collect();
+    loop {
+        let mut done = true;
+        for field_key in &field_keys {
+            if field_mapping[field_key].len() > 1 {
+                let disallowed_fields: HashSet<&str> = field_mapping
+                    .iter()
+                    .filter(|(k, v)| *k != field_key && v.len() == 1)
+                    .flat_map(|(_, v)| v.iter())
+                    .copied()
+                    .collect();
+
+                field_mapping
+                    .get_mut(field_key)
+                    .unwrap()
+                    .retain(|k| !disallowed_fields.contains(k));
+
+                done = false;
+            }
+        }
+
+        if done {
+            break;
+        }
+    }
+
+    field_mapping
+        .iter()
+        .filter(|(_, v)| v.iter().next().unwrap().starts_with("departure"))
+        .map(|(k, _)| your_ticket.get(*k).unwrap())
+        .product()
 }
 
 pub fn solve(lines: &[String]) -> Solution {
@@ -43,7 +134,6 @@ pub fn solve(lines: &[String]) -> Solution {
                 ParsePhase::Prelude => {
                     let mut sides = line.split(": ");
                     let lhs = sides.next().unwrap();
-                    dbg!(&lhs);
                     let rhs = sides.next().unwrap();
                     rules.insert(
                         lhs,
@@ -69,7 +159,7 @@ pub fn solve(lines: &[String]) -> Solution {
     }
 
     (
-        solve_a(&rules, &your_ticket, &other_tickets).to_string(),
-        "".to_string(),
+        solve_a(&rules, &other_tickets).to_string(),
+        solve_b(&rules, &your_ticket, &other_tickets).to_string(),
     )
 }
